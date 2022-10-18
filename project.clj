96516ccf46baac13b52ea28e9603cc77f4131f3b
Neo Lee <neo.lee@gmail.com>
2022-10-18 16:22:19 +0800

    Release 0.2.2, update zprint to avoid conflict

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject hashp "0.2.1"
+(defproject com.github.neolee/hashp "0.2.2"
   :description "A better \"prn\" for debugging"
-  :url "https://github.com/weavejester/hashp"
+  :url "https://github.com/neolee/hashp"
   :license {:name "The MIT License"
             :url "http://opensource.org/licenses/MIT"}
-  :dependencies [[org.clojure/clojure "1.10.2"]
+  :dependencies [[org.clojure/clojure "1.10.3"]
                  [org.clojure/core.rrb-vector "0.1.2"]
-                 [mvxcvi/puget "1.3.1"]
+                 [mvxcvi/puget "1.3.2"]
                  [clj-stacktrace "0.2.8"]
                  [net.cgrand/macrovich "0.2.1"]
                  [zprint "1.2.4"]])
